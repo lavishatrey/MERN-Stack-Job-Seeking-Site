43a6dff1787f3ff1961af730c8c2ade7f97230cc
lavishatrey <lavishatrey@gmail.com>
2024-06-29 20:48:08 +0530

    back to normal

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', // Change 'dist' to 'build'
-  },
-  plugins: [react()],// other configurations...
-});
+  plugins: [react()],
+})
